01755a0748e258f9641be21712e03a2b053be0f7
Wolfsanek2 <159615155+Wolfsanek2@users.noreply.github.com>
2024-03-31 15:35:37 +0300

    update main.cpp

--- module1/main.cpp
+++ module1/main.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
 
 void run(std::istream &input, std::ostream &output);
+void test_run();
 void test();
 
 int main() {
     
     test();
-    // run();
+    // run(std::cin, std::cout);
     
     return 0;
+}
+
+void run(std::istream &input, std::ostream &output) {
+    
+}
+
+void test_run() {
+    {
+        
+    }
+    std::cout << "run test OK" << std::endl;
+}
+
+void test() {
+    test_run();
 }
